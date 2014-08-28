71c51eaf00445b0702ed07d7878a1a32b9db4693
James Schüpbach <james.schuepach@gmail.com>
2014-08-28 17:00:57 +0200

    added plugin routes

---
+++ Config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+	Router::connect('/GpioWrapper/devices/:action',
+		array('plugin' => 'GpioWrapper', 'controller' => 'devices'));
+	Router::connect('/GpioWrapper/devices/',
+		array('plugin' => 'GpioWrapper', 'controller' => 'devices','action' => 'index'));
+		
+	Router::connect('/GpioWrapper/wateringHours/:action',
+		array('plugin' => 'GpioWrapper', 'controller' => 'wateringHours'));
+	Router::connect('/GpioWrapper/wateringHours/',
+		array('plugin' => 'GpioWrapper', 'controller' => 'wateringHours','action' => 'index'));
+?>

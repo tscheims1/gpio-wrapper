62f6bce275da0bfa636ee9f53ed11b6077649d23
James Schüpbach <james.schuepach@gmail.com>
2014-08-28 22:57:04 +0200

    added gpio communicator class

---
+++ Lib/GpioCommunicator.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This Class Communicate with the GPIO
+ * @author James Mayr
+ * @version 1.0
+ */ 
+class GpioCommunicator
+{
+	/**
+	 * Write a value on a specifc GPIO-Port
+	 * @param int $deviceNumber
+	 * @param boolean $value
+	 */
+	public function write($deviceNumber,$value)
+	{
+		$this->validateDeviceNumber($deviceNumber);
+		$this->validateValue($value);
+		
+		
+		exec("gpio -g write ".$deviceNumber." ".$value);
+	}
+	/**
+	 * read from a specific GPIO-Port
+	 * @param int $deviceNumber
+	 * @return boolean
+	 */
+	public function read($deviceNumber)
+	{
+		$this->validateDeviceNumber($deviceNumber);
+		
+		$value = exec("gpio -g read ".$deviceNumber);
+		return $value;
+	}
+	/**
+	 * Validate the deviceNumber
+	 * @param int deviceNumber
+	 * @throws Exeption
+	 */
+	private function validateDeviceNumber($deviceNumber)
+	{
+		if(!is_numeric($deviceNumber) || $deviceNumber < 0 || $deviceNumber > 26)
+		{
+			throw new Exception("invalid parameter: deviceNumber");
+		}	
+	}
+	/**
+	 * validate the given value
+	 * @param boolean value
+	 * @throws Exception
+	 */
+	private function validateValue($value)
+	{
+		if(!($value!= 1 || $value != 0))
+			throw new Exception("invalid parameter: value");
+	}
+}
